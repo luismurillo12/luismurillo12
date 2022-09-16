c9c11f7ca30706cfad5aa4223c6c21e5c40a0a80
AndreyAR7 <andyrt962@gmail.com>
2022-09-15 21:24:01 -0600

    Add first commit - tarea1

---
+++ app/src/main/java/com/lugares_j/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.lugares_j
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lugares_j.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    // Definicion del objeto para hacer la auntenticacion
+    private lateinit var auth : FirebaseAuth
+    private lateinit var binding : ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+}
+}
