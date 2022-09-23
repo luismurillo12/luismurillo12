bf063347865ac70838a47e54b75dd4ec9e3fdb74
AndreyAR7 <andyrt962@gmail.com>
2022-09-22 20:55:03 -0600

    Primera version del proyecto lugares --clase 2

--- app/src/main/java/com/lugares_j/MainActivity.kt
+++ app/src/main/java/com/lugares_j/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.lugares_j
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.lugares_j.databinding.ActivityMainBinding
@@ -16,9 +19,70 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // con esto se inicializa para manejar las vistas
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // con esto se inicializa Firebase para usarse en el app
+        //se asigna el objeto aith para la autenticacion
         FirebaseApp.initializeApp(this)
         auth = Firebase.auth
+
+        binding.btRegister.setOnClickListener{ haceRegistro () }
+        binding.btLogin.setOnClickListener{ haceLogin () }
+
 }
+    private fun haceRegistro() {
+        // optenemos la info que ingreso el usuario
+        val email = binding.etEmail.text.toString()
+        val clave = binding.etClave.text.toString()
+        // se llama a la funcion para crear un usuario en firebase (correo/contraseña)
+        auth.createUserWithEmailAndPassword(email,clave)
+            .addOnCompleteListener (this){ task->
+                var user: FirebaseUser? = null
+                if(task.isSuccessful){ // si logra crear el usuario
+                    Log.d("Autenticando","Usuario Creado")
+                     user = auth.currentUser  // recupero la info del usuario creado
+                }else{
+                    Log.d("Autenticando","Error creando usuario")
+                }
+                actualiza(user)
+            }
+    }
+
+
+    private fun haceLogin() {
+        // optenemos la info que ingreso el usuario
+        val email = binding.etEmail.text.toString()
+        val clave = binding.etClave.text.toString()
+        // se llama a la funcion para optener un usuario en firebase (correo/contraseña)
+        auth.signInWithEmailAndPassword(email,clave)
+            .addOnCompleteListener (this){ task->
+                var user: FirebaseUser? = null
+                if(task.isSuccessful){ // si logra crear el usuario
+                    Log.d("Autenticando","Usuario autenticado")
+                    user = auth.currentUser  // recupero la info del usuario creado
+                }else{
+                    Log.d("Autenticando","Error autenticando usuario")
+                }
+                actualiza(user)
+            }
+    }
+
+    private fun actualiza(user: FirebaseUser?) {
+        // si hay un usuario definido se pasa a la pantalla principal
+        if(user!=null){
+            //se pasa a la siguiente pantalla
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        }
+    }
+   //Se ejeculta cuando el app aparezca en la pantalla ...
+    public override fun onStart(){
+        super.onStart()
+       val usuario=auth.currentUser
+       actualiza(usuario)
+    }
+
 }
---
+++ app/src/main/java/com/lugares_j/ui/Lugar/LugarFragment.kt
@@ -0,0 +1,43 @@
+package com.lugares_j.ui.Lugar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.lugares_j.ViewModel.LugarViewModel
+import com.lugares_j.databinding.FragmentLugarBinding
+
+class LugarFragment : Fragment() {
+
+    private var _binding: FragmentLugarBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val lugarViewModel =
+            ViewModelProvider(this).get(LugarViewModel::class.java)
+
+        _binding = FragmentLugarBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        lugarViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
